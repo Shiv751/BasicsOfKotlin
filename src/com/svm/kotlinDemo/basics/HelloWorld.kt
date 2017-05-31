35dcc3c3011909194efa763a44461f92a71532a0
Shivam Bharadwaj <shivambhardwaj751@gmail.com>
2017-05-31 14:01:22 +0530

    Initial commit on 31/05/2017

---
+++ src/com/svm/kotlinDemo/basics/HelloWorld.kt
@@ -0,0 +1,4 @@
+package com.svm.kotlinDemo.basics
+fun main(a : Array<String>){
+    println("Hello India! You are my World!")
+}
---
+++ src/com/svm/kotlinDemo/basics/StringManipulation.kt
@@ -0,0 +1,6 @@
+package com.svm.kotlinDemo.basics
+fun main(a : Array<String>){
+    val str = StringBuffer("Shivam Bharadwaj")
+    val strOpr = str[8]
+    println("Character at index ${8} is $strOpr")
+}
